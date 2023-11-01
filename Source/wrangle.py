deb4a50a588332254f6e2efc7ab8986f7fbe0512
Ramiro-Lopez <lopez.j.ramiro1@gmail.com>
2023-11-01 12:54:29 -0500

    adding update to wrangle file

--- Source/wrangle.py
+++ Source/wrangle.py
@@ -6,3 +6,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 # Import seaborn for data visualization
 import seaborn as sns
+
+def acquire_test_data():
+    test_outpatient_df = pd.read_csv('Test_Outpatientdata-1542969243754.csv')
+    test_inpatient_df = pd.read_csv('Test_Inpatientdata-1542969243754.csv')
+    test_beneficiary_df = pd.read_csv('Test_Beneficiarydata-1542969243754.csv')
+    test_df = pd.read_csv('Test-1542969243754.csv')
+    return test_outpatient_df, test_beneficiary_df, test_inpatient_df, test_df
+
+def acquire_train_data():
+    train_outpatient_df = pd.read_csv('Train_Outpatientdata-1542865627584.csv')
+    train_inpatient_df = pd.read_csv('Train_Inpatientdata-1542865627584.csv')
+    train_benificiart_df = pd.read_csv('Train_Beneficiarydata-1542865627584.csv')
+    train_df = pd.read_csv('Train-1542865627584.csv')
+    return train_outpatient_df, train_inpatient_df, train_benificiart_df, train_df
