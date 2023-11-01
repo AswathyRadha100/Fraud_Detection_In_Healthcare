cef566eba61db49634ede9d849a8ed3a77c5d1ea
Ramiro-Lopez <lopez.j.ramiro1@gmail.com>
2023-11-01 13:57:58 -0500

    updateing file with acquire fuction

--- Source/wrangle.py
+++ Source/wrangle.py
@@ -8,15 +8,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def acquire_test_data():
-    test_outpatient_df = pd.read_csv('Test_Outpatientdata-1542969243754.csv')
-    test_inpatient_df = pd.read_csv('Test_Inpatientdata-1542969243754.csv')
-    test_beneficiary_df = pd.read_csv('Test_Beneficiarydata-1542969243754.csv')
-    test_df = pd.read_csv('Test-1542969243754.csv')
+    test_outpatient_df = pd.read_csv('Test_Outpatientdata.csv')
+    test_inpatient_df = pd.read_csv('Test_Inpatientdata.csv')
+    test_beneficiary_df = pd.read_csv('Test_Beneficiarydata.csv')
+    test_df = pd.read_csv('Test.csv')
     return test_outpatient_df, test_beneficiary_df, test_inpatient_df, test_df
 
 def acquire_train_data():
-    train_outpatient_df = pd.read_csv('Train_Outpatientdata-1542865627584.csv')
-    train_inpatient_df = pd.read_csv('Train_Inpatientdata-1542865627584.csv')
-    train_benificiart_df = pd.read_csv('Train_Beneficiarydata-1542865627584.csv')
-    train_df = pd.read_csv('Train-1542865627584.csv')
-    return train_outpatient_df, train_inpatient_df, train_benificiart_df, train_df
+    train_outpatient_df = pd.read_csv('Train_Outpatientdata.csv')
+    train_inpatient_df = pd.read_csv('Train_Inpatientdata.csv')
+    train_beneficiary_df = pd.read_csv('Train_Beneficiarydata.csv')
+    train_df = pd.read_csv('Train.csv')
+    return train_outpatient_df, train_inpatient_df, train_beneficiary_df, train_df
