c8fbabe80a4792b289e19b7fbaf6d1410bd1743b
Aswathy Radha <aswathy.radha@gmail.com>
2023-11-01 11:14:08 -0500

    Adding the folder

---
+++ Source/wrangle.py
@@ -0,0 +1,8 @@
+# Import numpy for numerical operations
+import pandas as pd
+# Import Pandas for data manipulation
+import numpy as np
+# Import Matplotlib for data visualization
+import matplotlib.pyplot as plt
+# Import seaborn for data visualization
+import seaborn as sns
